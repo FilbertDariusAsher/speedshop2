0e8e1831ebf7fc5eb360ade89f3b36dc6c053ba1
Rimuru <filbertasherdarius@gmail.com>
2026-05-19 14:50:28 +0700

    update index.php

--- sistem-penjualan/api/index.php
+++ sistem-penjualan/api/index.php
@@ -1,2 +1,14 @@
 <?php
+$storagePaths = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+];
+
+foreach ($storagePaths as $path) {
+    if (!is_dir($path)) {
+        mkdir($path, 0755, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
